146e478d2d5523aa82a286bc84a1fc692999a70e
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2022-04-15 13:33:31 -0700

    updating 0-setup_web_static.sh file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared
 echo "Deployment test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR $USER:$USER /data/
