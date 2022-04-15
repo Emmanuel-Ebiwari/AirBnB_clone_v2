699b89d71ae3f0a7a5856a7c36f5dc61e843119e
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2022-04-15 12:28:05 -0700

    updating 0-setup_web_static.sh file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,7 @@ sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 
 sudo mkdir -p /data/web_static/releases/test/
-sudo echo > "Deployment test" /data/web_static/releases/test/index.html
+echo "Deployment test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown $USER:$USER /data/
 sudo sed -i "53i\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
