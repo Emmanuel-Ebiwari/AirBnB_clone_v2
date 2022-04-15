0aaf0ea14eca449f686f4002a9d879bcf50be5fa
Emmanuel-Ebiwari <ebiwariemma@gmail.com>
2022-04-15 16:50:18 -0700

    adding 1-pack_web_static.py file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+a Fabric script that generates a .tgz archive from the contents of the
+web_static folder of your AirBnB, using the function do_pack
+"""
+
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """"creating a .tgz archive of the AirBnB web_static folder 
+        with a timestamp
+    """
+    date = datetime.now()
+    timestamp = date.strftime("%Y%m%d%H%M%S")
+
+    local("mkdir -p versions")
+    tgzFile = "versions/web_static_{}.tgz".format(timestamp)
+    tar = local("tar -cvzf {} web_static".format(tgzFile))
+
+    if tar.failed:
+        return None
+    return tgzFile
